96d1e291d26e3005e9a6a1c4d23af77423f4302b
sharmashashwat271 <104565730+sharmashashwat271@users.noreply.github.com>
2023-01-12 01:14:45 +0530

    Add initial website files

---
+++ cart-alter.js
@@ -0,0 +1,5 @@
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('container-fluid')[0]
+  var cartRows = cartItemContainer.getElementsByClassName('row')
+  for (var i = 0; i <CartRows.length; i++)
+}
